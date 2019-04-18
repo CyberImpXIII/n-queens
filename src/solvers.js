53f3fc5e3fa628d4b98a4f83a7ebc9a78635a16f
CyberImpXIII <jnelsenepstein@gmail.com>
2019-04-18 16:01:04 -0500

    built something that takes our n and generates every possible unique row, then used heaps algorythm to find every permutation of those unique rows, called a test that makes sure that those new permutations pass the tests and returns a number or board. encountering SOME problems with that though

--- src/solvers.js
+++ src/solvers.js
@@ -16,15 +16,104 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution; //fixme
+  // loop n-times. 
+  // let board = solution.rows();
+  let boardBank = []; 
+  let untouchRow = Array(n).fill(0); 
+  
+  for (let i =0; i < n; i++) { 
+    let tempArray = untouchRow.slice(); 
+    tempArray[i] = 1; 
+    boardBank.push(tempArray.slice()); 
+  }
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  var swap = function (array, pos1, pos2) {
+    var temp = array[pos1];
+    array[pos1] = array[pos2];
+    array[pos2] = temp;
+  };
+  
+  var heapsPermute = function (array, output, n = array.length) {
+    //n = n || array.length; // set n default to array.length
+    if (n === 1) {
+      output(array);
+    } else {
+      for (var i = 1; i <= n; i += 1) {
+        heapsPermute(array, output, n - 1);
+        if (n % 2) {
+          var j = 1;
+        } else {
+          var j = i;
+        }
+        swap(array, j - 1, n - 1); // -1 to account for javascript zero-indexing
+      }
+    }
+  };
+  
+
+  // For testing:
+  var solve = function(input){
+    var tempBoard = new Board(input);
+    if(tempBoard.hasAnyRooksConflicts() === false){
+      solution = tempBoard; 
+    }
+  }
+  
+  heapsPermute(boardBank, solve);
+
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution), solution);
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+
+  // loop n-times. 
+  // let board = solution.rows();
+  let boardBank = []; 
+  let untouchRow = Array(n).fill(0); 
+  
+  for (let i =0; i < n; i++) { 
+    let tempArray = untouchRow.slice(); 
+    tempArray[i] = 1; 
+    boardBank.push(tempArray.slice()); 
+  }
+
+  var swap = function (array, pos1, pos2) {
+    var temp = array[pos1];
+    array[pos1] = array[pos2];
+    array[pos2] = temp;
+  };
+  
+  var heapsPermute = function (array, output, n = array.length) {
+    //n = n || array.length; // set n default to array.length
+    if (n === 1) {
+      output(array);
+    } else {
+      for (var i = 1; i <= n; i += 1) {
+        heapsPermute(array, output, n - 1);
+        if (n % 2) {
+          var j = 1;
+        } else {
+          var j = i;
+        }
+        swap(array, j - 1, n - 1); // -1 to account for javascript zero-indexing
+      }
+    }
+  };
+  
+
+  // For testing:
+  var solve = function(input){
+    var tempBoard = new Board(input);
+    if(tempBoard.hasAnyRooksConflicts() === false){
+      solutionCount ++; 
+    }
+  }
+  
+  heapsPermute(boardBank, solve);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -32,10 +121,55 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var solutionCount = 0; //fixme
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  // loop n-times. 
+  // let board = solution.rows();
+  let boardBank = []; 
+  let untouchRow = Array(n).fill(0); 
+  
+  for (let i =0; i < n; i++) { 
+    let tempArray = untouchRow.slice(); 
+    tempArray[i] = 1; 
+    boardBank.push(tempArray.slice()); 
+  }
+
+  var swap = function (array, pos1, pos2) {
+    var temp = array[pos1];
+    array[pos1] = array[pos2];
+    array[pos2] = temp;
+  };
+  
+  var heapsPermute = function (array, output, n = array.length) {
+    //n = n || array.length; // set n default to array.length
+    if (n === 1) {
+      output(array);
+    } else {
+      for (var i = 1; i <= n; i += 1) {
+        heapsPermute(array, output, n - 1);
+        if (n % 2) {
+          var j = 1;
+        } else {
+          var j = i;
+        }
+        swap(array, j - 1, n - 1); // -1 to account for javascript zero-indexing
+      }
+    }
+  };
+  
+
+  // For testing:
+  var solve = function(input){
+    var tempBoard = new Board(input);
+    if(tempBoard.hasAnyQueensConflicts() === false){
+      solutionCount ++; 
+    }
+  }
+  
+  heapsPermute(boardBank, solve);
+
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solutionCount));
+  return solutionCount;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
